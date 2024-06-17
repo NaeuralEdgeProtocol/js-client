b452b9b85658a0b17781f8468fd138111ce55e35
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-06-17 10:42:41 +0300

    fix: DCT imports

--- src/utils/dcts/index.js
+++ src/utils/dcts/index.js
@@ -2,5 +2,5 @@ export { DCT_TYPE_META_STREAM, schema as metaStreamDCTSchema } from './meta.stre
 export { DCT_TYPE_VOID_STREAM, schema as voidDCTSchema } from './void.dct.js';
 export { DCT_TYPE_VIDEO_FILE, schema as videoFileDCTSchema } from './video.file.dct.js';
 export { DCT_TYPE_VIDEO_STREAM, schema as videoStreamDCTSchema } from './video.stream.dct.js';
-export { DCT_TYPE_ON_DEMAND_TEXT_INPUT, schema as onDemandTextInputSchema } from './on.demand.text.input.dct';
-export { DCT_TYPE_ON_DEMAND_INPUT, schema as onDemandInputSchema } from './on.demand.input.dct';
+export { DCT_TYPE_ON_DEMAND_TEXT_INPUT, schema as onDemandTextInputSchema } from './on.demand.text.input.dct.js';
+export { DCT_TYPE_ON_DEMAND_INPUT, schema as onDemandInputSchema } from './on.demand.input.dct.js';
