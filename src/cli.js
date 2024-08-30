d35e644254a3c777c1b0bcf746bfb62c56adfe24
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-08-30 16:10:52 +0300

    fix: added missing export as pem to CLI;

--- src/cli.js
+++ src/cli.js
@@ -18,6 +18,7 @@ const hexPair = {
     publicKey: keyPair.publicKey.export({ type: 'spki', format: 'der' }).toString('hex'),
     privateKey: keyPair.privateKey.export({ type: 'pkcs8', format: 'der' }).toString('hex'),
     naeuralAddr: NaeuralBC.addressFromPublicKey(keyPair.publicKey),
+    pem: keyPair.privateKey.export({ type: 'pkcs8', format: 'pem' }),
 };
 
 console.log('=-=-=-=-=-=-=-= NaeuralEdgeProtocol Blockchain Address Generator =-=-=-=-=-=-=-=');
@@ -25,6 +26,7 @@ console.log(`   Your Address: ${hexPair.naeuralAddr}`);
 console.log('--------------------------------------------------------------------');
 console.log(`   Your Public Key: ${hexPair.publicKey}`);
 console.log(`   Your PRIVATE Key: ${hexPair.privateKey}`);
+console.log(`   Your PRIVATE Key PEM: ${hexPair.pem}`);
 console.log('--------------------------------------------------------------------');
 console.log('   Use these keys to setup the blockchain engine. The public/private keypair should be stored in');
 console.log('   a SAFE place, and **should not** be committed to git. The address can also be deduced from the');
